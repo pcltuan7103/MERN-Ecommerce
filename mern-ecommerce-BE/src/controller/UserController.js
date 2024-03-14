3a1dfc52a948b52fb99a843590dbe2340ea86111
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-03-14 14:40:39 +0700

    Create API Register

--- mern-ecommerce-BE/src/controller/UserController.js
+++ mern-ecommerce-BE/src/controller/UserController.js
@@ -2,9 +2,28 @@ const userService = require('../service/UserService')
 
 const createUser = async (req, res) => {
     try{
-        console.log(req.body)
-        const res = await userService.createUser()
-        return res.status(200).json()
+        const {name, email, password, confirmPassword, phone} = req.body
+        const reg = /^\w+([-+.']\w+)*@w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const isCheckEmail = reg.test(email)
+        if(!name || !email || !password || !confirmPassword || !phone){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            })
+        }else if(!isCheckEmail){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is email'
+            })
+        }else if(password !== confirmPassword){
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The password is equal confirmPassword'
+            })
+        }
+
+        const respone = await userService.createUser(req.body)
+        return res.status(200).json(respone)
     } catch (e) {
         return res.status(404).json({
             message: e
--- mern-ecommerce-BE/src/service/UserService.js
+++ mern-ecommerce-BE/src/service/UserService.js
@@ -1,7 +1,32 @@
+const Account = require('../model/AccountModel')
+
 const createUser = () => {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
+        const {name, email, password, confirmPassword, phone} = newUser
         try{
-            resolve({})
+            const checkAccount = await Account.findOne({
+                email: email
+            })
+            if(checkAccount !== null){
+                resolve({
+                    status: 'OK',
+                    message: 'The email is already'
+                })
+            }
+            const createUser = await Account.create({
+                name, 
+                email, 
+                password, 
+                confirmPassword, 
+                phone
+            })
+            if(createUser){
+                resolve({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    date: createUser
+                })
+            }
         } catch (e) {
             reject(e)
         }
